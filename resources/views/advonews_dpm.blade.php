dc57d55d7c20da903221738b7685116462f11098
bagusbayu <bagusbayu93@gmail.com>
2020-08-31 23:37:15 +0700

    Commit Update F

--- resources/views/advonews_dpm.blade.php
+++ resources/views/advonews_dpm.blade.php
@@ -7,15 +7,19 @@
          
         <div class="row">
           <div class="col-lg-12">
-             <div class="section-heading">
-             <!-- <a href="#">Read All News</a> -->
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="section-heading">
+                  <h2 class="text-black">Advo News</h2>
+                </div>
+              </div>
             </div>
             <div class="row">
+
               <div class="col-lg-6">
-                <div class="post-entry-big">
-              <h2 class="text-black">Advo News</h2>
               @foreach($data as $datas)
-                  <a href="#" class="img-link"><img src="{{ url('uploads/file/'.$datas->pict) }}" alt="Image" class="img-fluid"></a>
+                <div class="post-entry-big">
+                  <a href="#" class="img-link"><img src="{{ url('uploads/file/'.$datas->pict) }}" alt="Image" class="img-fluid" style="width:540px;height:359.7px"></a>
                   <div class="post-content">
                     <div class="post-meta"> 
                       <!--<a href="#">June 6, 2019</a>
@@ -23,13 +27,14 @@
                       <h3 class="post-heading"><a href="#">{{$datas->judul}}</a></h3>
                     </div>
                     <a href="{{ url('uploads/file/'.$datas->file) }}">Download File</a>
-                  </div> @endforeach
-                </div>
+                  </div> 
+                </div>@endforeach
               </div>
+
               <div class="col-lg-6">
+              @foreach($iya as $iyas)
                 <div class="post-entry-big horizontal d-flex mb-4">
-                @foreach($iya as $iyas)
-                  <a href="news-single.html" class="img-link mr-4"><img src="{{ url('uploads/file/'.$iyas->pict) }}" alt="Image" class="img-fluid"></a>
+                  <a href="news-single.html" class="img-link mr-4"><img src="{{ url('uploads/file/'.$iyas->pict) }}" alt="Image" class="img-fluid"  style="width:90px;height:89.77px"></a>
                   <div class="post-content">
                     <div class="post-meta">
                     <h3 class="post-heading"><a href="news-single.html">{{$iyas->judul}}</a></h3>
@@ -37,33 +42,9 @@
                       <span class="mx-1">/</span>
                       <a href="#">Admission</a>, --> <a href="{{ url('uploads/file/'.$iyas->file) }}">Download File</a>
                     </div>
-                    @endforeach
                   </div>
                 </div>
-
-                <div class="post-entry-big horizontal d-flex mb-4">
-                <!--  <a href="news-single.html" class="img-link mr-4"><img src="assets/images/blog_2.jpg" alt="Image" class="img-fluid"></a>
-                  <div class="post-content">
-                    <div class="post-meta">
-                      <a href="#">June 6, 2019</a>
-                      <span class="mx-1">/</span>
-                      <a href="#">Admission</a>, <a href="#">Updates</a>
-                    </div>
-                    <h3 class="post-heading"><a href="news-single.html">Campus Camping and Learning Session</a></h3>
-                  </div> -->
-                </div>
-
-                <div class="post-entry-big horizontal d-flex mb-4"> <!--
-                  <a href="news-single.html" class="img-link mr-4"><img src="assets/images/blog_1.jpg" alt="Image" class="img-fluid"></a>
-                  <div class="post-content">
-                    <div class="post-meta">
-                      <a href="#">June 6, 2019</a>
-                      <span class="mx-1">/</span>
-                      <a href="#">Admission</a>, <a href="#">Updates</a>
-                    </div>
-                    <h3 class="post-heading"><a href="news-single.html">Campus Camping and Learning Session</a></h3>
-                  </div> -->
-                </div> 
+                @endforeach
               </div>
             </div>
           </div>
